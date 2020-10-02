d3837d73272ce1b744ccfcf4706617c8baae36bc
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-02 10:27:51 +0200

    Macropahge movmeent

---
+++ TP-ECS/Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Move : MonoBehaviour
+{
+    public float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+            movement += Vector3.left;
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+            movement += Vector3.right;
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+            movement += Vector3.up;
+         if (Input.GetKey(KeyCode.DownArrow) == true)
+            movement += Vector3.down;
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
