e9b0c68381387a02d01879b5f9c3b877edebb407
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-02 11:09:03 +0200

    Generation Virus

---
+++ TP-ECS/Assets/Scripts/VirusGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class VirusGenerator : MonoBehaviour
+{
+    public GameObject virusPrefab;
+
+    public float reloadTime = 2f;
+    public float reloadProgress = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        reloadProgress += Time.deltaTime;
+        if(reloadProgress >= reloadTime)
+        {
+            Instantiate<GameObject>(virusPrefab);
+            reloadProgress = 0;
+        }
+    }
+}
